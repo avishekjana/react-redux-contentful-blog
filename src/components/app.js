645492118b3987262aac8177127a4ad9c7df9ea3
Avishek Jana <avishekjana.it@gmail.com>
2017-04-08 18:14:19 +0530

    First component added

--- src/components/app.js
+++ src/components/app.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        {this.props.children}
-      </div>
+      <div>{this.props.children}</div>
     );
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/posts_index';
-
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex}/>
