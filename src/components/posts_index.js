61c101b20c70165cbed3aded392280772239ec5b
Avishek Jana <avishekjana.it@gmail.com>
2017-04-08 20:09:27 +0530

    Posts displayed properly.

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
   componentWillMount() {
     this.props.fetchPosts();
   }
+  renderPosts() {
+    return this.props.posts.map((post, index) => {
+      return (
+        <article key={post.sys.id}>
+          <h3 className="major">{post.fields.title}</h3>
+          <p>{post.fields.description}</p>
+        </article>
+      );
+    });
+  }
   render() {
-    return <div> Our blog posts will come here.</div>;
+    return (
+      <div>
+        <h2>Blog Posts</h2>
+        {this.renderPosts()}
+      </div>
+    );
   }
 }
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
+}
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
