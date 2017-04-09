28f34609800b5fb266757d48a676544c22bcf040
Avishek Jana <avishekjana.it@gmail.com>
2017-04-09 17:59:08 +0530

    Server PORT changed to 3000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.port || 9020;
+const port = process.env.port || 3000;
 const app = express();
 
 app.use(express.static(__dirname));
